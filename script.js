29de0c9e93e81b6b1f687c8c3349dbf86656dcb3
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2023-03-22 15:19:53 +0000

    Added a dropdown to change pages

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ document.getElementById('generate').onclick = function () {
 	html2pdf(element);
 };
 
-$(function(){
-	$("#invoice").load("../templates/invoice.html"); 
-});
+//$(function(){
+//	$("#invoice").load("../templates/invoice.html"); 
+//});
+
+// $(function() {
+// 	$("#pageSelect").on("change", function() {
+// 	  var pageUrl = $(this).val();
+// 	  $("#invoice").load("../templates/template2.html"); 
+// 	});
+	
+// 	$("#invoice").load("../templates/template1.html"); 
+//   });
+
+
+// jQuery(function() {
+// 	jQuery("#pageSelect").on("change", function() {
+// 	  var pageUrl = jQuery(this).val();
+// 	  console.log(pageUrl);
+// 	  jQuery("#invoice").load(pageUrl);
+// 	});
+  
+// 	jQuery("#invoice").load("../templates/template1.html");
+//   });
+  
+jQuery($ => {
+	$('#pageSelect').on('change', function(e) {
+	  var pageUrl = $(this).val(); 
+	  console.log(pageUrl)
+	  $('#invoice').load(pageUrl);
+	});
+  
+	$("#invoice").load("../templates/template1.html");
+  });
