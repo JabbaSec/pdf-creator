1b30990de7a5a4b5e4525a3ed9182f232efca554
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2023-03-26 21:38:41 +0100

    Moved code into script.js

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ jQuery($ => {
   
 	$("#invoice").load("../templates/template1.html");
 });
+
+var planTitle = document.getElementById("planTitle")
+var planDescription = document.getElementById("planDescription")
+var planPrice = document.getElementById("planPrice")
+var subtotal = document.getElementById("subtotal")
+var amountPaid = document.getElementById("amountPaid")
+var subscriptionSelected = document.getElementById("subscriptionSelect")
+
+subscriptionSelected.addEventListener("change", updateSubscription)
+
+function updateSubscription() {
+  if (subscriptionSelected.value == "monthly") {
+    planTitle.innerHTML = "Monthly"
+    planDescription.innerHTML = "Monthly USD"
+    planPrice.innerHTML = "$10.00"
+    subtotal.innerHTML = "$10.00"
+    amountPaid.innerHTML = "$10.00"
+  }
+
+  if (subscriptionSelected.value == "annual") {
+    planTitle.innerHTML = "Annual"
+    planDescription.innerHTML = "Annual USD"
+    planPrice.innerHTML = "$90.00"
+    subtotal.innerHTML = "$90.00"
+    amountPaid.innerHTML = "$90.00"
+  }
+}
